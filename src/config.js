19584d057371dd44128b2c7083d154f5a3f484a6
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-05-23 14:38:39 -0700

    hopefully this works

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL
+  DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://postgres@localhost/spaced-repetition',
-  TEST_DATABASE_URL: process.env.TEST_DB_URL || "postgresql://postgres@localhost/spaced-repetition-test",
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/spaced-repetition-test",
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
